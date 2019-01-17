2acdfb333e2234ad6275e44f86317379d5be4e71
DhevaMargaPutra <dhevamp57@gmail.com>
2019-01-17 11:10:08 +0700

    keduax

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// const opening = document.getElementById('opening-id').style.animationPlayState = "running";
-
 function opening() {
     document.getElementById('opening-id').style.animationPlayState = "running";
 }

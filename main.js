c9acfd9c6e94fe37dfaed8cd5a5880c42b94264b
DhevaMargaPutra <dhevamp57@gmail.com>
2019-01-17 09:25:48 +0700

    pertamax

---
+++ main.js
@@ -0,0 +1,5 @@
+// const opening = document.getElementById('opening-id').style.animationPlayState = "running";
+
+function opening() {
+    document.getElementById('opening-id').style.animationPlayState = "running";
+}

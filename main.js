1f5af616abdaec616c35586d508225e64929ed7e
DhevaMargaPutra <dhevamp57@gmail.com>
2019-01-18 21:23:44 +0700

    tambah_materi

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const lanjutKeMateriBtn = document.getElementById('nav-btn-materi');
+lanjutKeMateriBtn.addEventListener('click', function() {
+    
+});
+
 function opening() {
     document.getElementById('opening-id').style.animationPlayState = "running";
 }

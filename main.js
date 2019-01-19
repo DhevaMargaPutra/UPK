4cb78e24c97d9da791a4f0c1f7db1e2ade5ed0dc
DhevaMargaPutra <dhevamp57@gmail.com>
2019-01-19 07:30:00 +0700

    fix_opening

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-const lanjutKeMateriBtn = document.getElementById('nav-btn-materi');
-lanjutKeMateriBtn.addEventListener('click', function() {
-    
-});
+//cek udah pernah buka app ini? lewati : set sessionStorage
+if (sessionStorage.key == null) {
+    sessionStorage.setItem('hasLogin', JSON.stringify(false));
+}
 
+//DOM
+const openingAnim = document.getElementById('opening-id');
+
+//cek udah pernah login? gak usah tampil opening : tampil
+if (JSON.parse(sessionStorage.getItem('hasLogin')) == true) {
+    openingAnim.style.display = 'none';
+}
+
+//nge-play animasi opening & set sudah login
 function opening() {
-    document.getElementById('opening-id').style.animationPlayState = "running";
+    openingAnim.style.animationPlayState = 'running';
+    sessionStorage.setItem('hasLogin', JSON.stringify(true));
 }

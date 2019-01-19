4302e5b0c6e16552fcf85be1280213ba24cf6115
DhevaMargaPutra <dhevamp57@gmail.com>
2019-01-19 21:13:22 +0700

    up_button00

--- materi.js
+++ materi.js
@@ -1,9 +1,5 @@
+//auto generate warna setiap bab
 const divMateri = document.getElementsByClassName('materi');
-
-// divCerah.style.backgroundColor = '#343a40';
-// childDivCerah[0].style.color = '#f8f9fa';
-// childDivCerah[1].style.color = '#f8f9fa';
-
 for (let i = 0; i < divMateri.length; i++) {
     if ((i % 2) == 0) {
         //cerah
@@ -15,5 +11,17 @@ for (let i = 0; i < divMateri.length; i++) {
         divMateri[i].style.backgroundColor = '#343a40';
         divMateri[i].children[0].style.color = '#f8f9fa';
         divMateri[i].children[1].style.color = '#f8f9fa';
+    }
+}
+
+//show hide up button
+const upButton = document.getElementById('up-button');
+function checkScroll() {
+    if (scrollY > 300) {
+        upButton.style.display = 'initial';
+        console.log('show');
+    } else {
+        upButton.style.display = 'none';
+        console.log('hide');
     }
 }
